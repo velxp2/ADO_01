4dac5c9615bcc52f7c5e9663f1af5648cefd34df
victo <victo@NB_SERAFIM>
2018-09-01 13:51:43 -0300

    Classes de conexão, servico, produto e dao prontas.

---
+++ PG_ADO_01/src/main/java/br/com/ado/db/dao/DaoProduto.java
@@ -0,0 +1,311 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ado.db.dao;
+
+import br.com.ado.db.util.ConnectionUtils;
+import br.com.ado.model.produto.Produto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author victo
+ */
+public class DaoProduto {
+    
+    public static void inserir(Produto produto)
+            throws SQLException, Exception {
+        //Monta a string de inserção de um cliente no BD,
+        //utilizando os dados do clientes passados como parâmetro
+        String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO, PRECO_COMPRA, PRECO_VENDA, QUANTIDADE) VALUES (?,?,?,?,?)";
+        //Conexão para abertura e fechamento
+        Connection connection = null;
+        //Statement para obtenção através da conexão, execução de
+        //comandos SQL e fechamentos
+        PreparedStatement preparedStatement = null;
+        try {
+            //Abre uma conexão com o banco de dados
+            connection = ConnectionUtils.getConnection();
+            //Cria um statement para execução de instruções SQL
+            preparedStatement = connection.prepareStatement(sql);
+            //Configura os parâmetros do "PreparedStatement"
+            preparedStatement.setString(1, produto.getNome());
+            preparedStatement.setString(2, produto.getDescricao());
+            preparedStatement.setFloat(3, produto.getPreco_compra());
+            preparedStatement.setFloat(4, produto.getPreco_venda());
+            preparedStatement.setFloat(5, produto.getQuantidade());
+            //Executa o comando no banco de dados
+            preparedStatement.execute();
+        } finally {
+            //Se o statement ainda estiver aberto, realiza seu fechamento
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
+    
+    public static List<Produto> procurar(String valor)
+            throws SQLException, Exception {
+        //Monta a string de consulta de clientes no banco, utilizando
+        //o valor passado como parâmetro para busca nas colunas de
+        //nome ou sobrenome (através do "LIKE" e ignorando minúsculas
+        //ou maiúsculas, através do "UPPER" aplicado à coluna e ao
+        //parâmetro). Além disso, também considera apenas os elementos
+        //que possuem a coluna de ativação de clientes configurada com
+        //o valor correto ("enabled" com "true")
+        String sql = "SELECT * FROM Produto WHERE ((UPPER(nome) LIKE UPPER(?))";
+        //Lista de clientes de resultado
+        List<Produto> listaProduto = null;
+        //Conexão para abertura e fechamento
+        Connection connection = null;
+        //Statement para obtenção através da conexão, execução de
+        //comandos SQL e fechamentos
+        PreparedStatement preparedStatement = null;
+        //Armazenará os resultados do banco de dados
+        ResultSet result = null;
+        try {
+            //Abre uma conexão com o banco de dados
+            connection = ConnectionUtils.getConnection();
+            //Cria um statement para execução de instruções SQL
+            preparedStatement = connection.prepareStatement(sql);
+            //Configura os parâmetros do "PreparedStatement"
+            preparedStatement.setString(1, "%" + valor + "%");
+            //Executa a consulta SQL no banco de dados
+            result = preparedStatement.executeQuery();
+
+            //Itera por cada item do resultado
+            while (result.next()) {
+                //Se a lista não foi inicializada, a inicializa
+                if (listaProduto == null) {
+                    listaProduto = new ArrayList<Produto>();
+                }
+                Produto produto = new Produto();
+                
+                produto.setNome(result.getString("PRODUTO.NOME"));
+                produto.setDescricao(result.getString("DESCRICAO"));
+                produto.setPreco_compra(result.getFloat("PRECO_COMPRA"));
+                produto.setPreco_venda(result.getFloat("PRECO_VENDA"));
+                produto.setQuantidade(result.getInt("QUANTIDADE"));
+                produto.setDt_cadastro(result.getDate("DT_CADASTRO"));
+                produto.setCategoria(result.getString("CATEGORIA.NOME"));
+                //Adiciona a instância na lista
+                listaProduto.add(produto);
+            }
+        } finally {
+            //Se o result ainda estiver aberto, realiza seu fechamento
+            if (result != null && !result.isClosed()) {
+                result.close();
+            }
+            //Se o statement ainda estiver aberto, realiza seu fechamento
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+        //Retorna a lista de clientes do banco de dados
+        return listaProduto;
+    }
+
+    
+    public static void atualizar(Produto produto, int id)
+            throws SQLException, Exception {
+        //Monta a string de atualização do cliente no BD, utilizando
+        //prepared statement
+        String sql = "UPDATE PRODUTO SET NOME=?, DESCRICAO=?, PRECO_COMPRA=?, PRECO_VENDA=?, QUANTIDADE=?"
+                + "WHERE (id=?)";
+        //Conexão para abertura e fechamento
+        Connection connection = null;
+        //Statement para obtenção através da conexão, execução de
+        //comandos SQL e fechamentos
+        PreparedStatement preparedStatement = null;
+        try {
+            //Abre uma conexão com o banco de dados
+            connection = ConnectionUtils.getConnection();
+            //Cria um statement para execução de instruções SQL
+            preparedStatement = connection.prepareStatement(sql);
+            //Configura os parâmetros do "PreparedStatement"
+            preparedStatement.setString(1, produto.getNome());
+            preparedStatement.setString(2, produto.getDescricao());
+            preparedStatement.setFloat(3, produto.getPreco_compra());
+            preparedStatement.setFloat(4, produto.getPreco_venda());
+            preparedStatement.setFloat(5, produto.getQuantidade());
+            preparedStatement.setFloat(6, id);
+            
+
+            //Executa o comando no banco de dados
+            preparedStatement.execute();
+        } finally {
+            //Se o statement ainda estiver aberto, realiza seu fechamento
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
+    
+    public static void excluir(String nome) throws SQLException, Exception {
+        //Monta a string de atualização do cliente no BD, utilizando
+        //prepared statement
+        String sql = "DELETE FROM Produto  WHERE NOME=?";
+        //Conexão para abertura e fechamento
+        Connection connection = null;
+        //Statement para obtenção através da conexão, execução de
+        //comandos SQL e fechamentos
+        PreparedStatement preparedStatement = null;
+        try {
+            //Abre uma conexão com o banco de dados
+            connection = ConnectionUtils.getConnection();
+            //Cria um statement para execução de instruções SQL
+            preparedStatement = connection.prepareStatement(sql);
+            //Configura os parâmetros do "PreparedStatement"
+            preparedStatement.setString(1, nome);
+
+            //Executa o comando no banco de dados
+            preparedStatement.execute();
+        } finally {
+            //Se o statement ainda estiver aberto, realiza seu fechamento
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
+    
+    public static Produto obter(String nome)
+            throws SQLException, Exception {
+        //Compõe uma String de consulta que considera apenas o cliente
+        //com o ID informado e que esteja ativo ("enabled" com "true")
+        String sql = "SELECT * FROM Produto WHERE (nome=?)";
+
+        //Conexão para abertura e fechamento
+        Connection connection = null;
+        //Statement para obtenção através da conexão, execução de
+        //comandos SQL e fechamentos
+        PreparedStatement preparedStatement = null;
+        //Armazenará os resultados do banco de dados
+        ResultSet result = null;
+        try {
+            //Abre uma conexão com o banco de dados
+            connection = ConnectionUtils.getConnection();
+            //Cria um statement para execução de instruções SQL
+            preparedStatement = connection.prepareStatement(sql);
+            //Configura os parâmetros do "PreparedStatement"
+            preparedStatement.setString(1, nome);
+
+            //Executa a consulta SQL no banco de dados
+            result = preparedStatement.executeQuery();
+
+            //Verifica se há pelo menos um resultado
+            if (result.next()) {
+                //Cria uma instância de Cliente e popula com os valores do BD
+                Produto produto = new Produto();
+                produto.setNome(result.getString("NOME"));
+                produto.setDescricao(result.getString("DESCRICAO"));
+                produto.setPreco_compra(result.getFloat("PRECO_COMPRA"));
+                produto.setPreco_venda(result.getFloat("PRECO_VENDA"));
+                produto.setQuantidade(result.getInt("QUANTIDADE"));
+                //Retorna o resultado
+                return produto;
+            }
+        } finally {
+            //Se o result ainda estiver aberto, realiza seu fechamento
+            if (result != null && !result.isClosed()) {
+                result.close();
+            }
+            //Se o statement ainda estiver aberto, realiza seu fechamento
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+        //Se chegamos aqui, o "return" anterior não foi executado porque
+        //a pesquisa não teve resultados
+        //Neste caso, não há um elemento a retornar, então retornamos "null"
+        return null;
+    }
+    public static List<Produto> listar()
+            throws SQLException, Exception {
+        //Monta a string de listagem de clientes no banco, considerando
+        //apenas a coluna de ativação de clientes ("enabled")
+        String sql = "SELECT PRODUTO.NOME, DESCRICAO, PRECO_COMPRA, PRECO_VENDA, QUANTIDADE, DT_CADASTRO, CATEGORIA.NOME FROM PRODUTOBD.PRODUTO" +
+                    "INNER JOIN PRODUTOBD.PRODUTO_CATEGORIA ON PRODUTOBD.PRODUTO.ID = PRODUTOBD.PRODUTO_CATEGORIA.ID_PRODUTO" +
+                    "INNER JOIN PRODUTOBD.CATEGORIA ON PRODUTOBD.PRODUTO_CATEGORIA.ID_CATEGORIA = PRODUTOBD.CATEGORIA.ID;";
+        //Lista de clientes de resultado
+        List<Produto> listaProduto = null;
+        //Conexão para abertura e fechamento
+        Connection connection = null;
+        //Statement para obtenção através da conexão, execução de
+        //comandos SQL e fechamentos
+        PreparedStatement preparedStatement = null;
+        //Armazenará os resultados do banco de dados
+        ResultSet result = null;
+        try {
+            //Abre uma conexão com o banco de dados
+            connection = ConnectionUtils.getConnection();
+            //Cria um statement para execução de instruções SQL
+            preparedStatement = connection.prepareStatement(sql);
+
+            //Executa a consulta SQL no banco de dados
+            result = preparedStatement.executeQuery();
+
+            //Itera por cada item do resultado
+            while (result.next()) {
+                //Se a lista não foi inicializada, a inicializa
+                if (listaProduto == null) {
+                    listaProduto = new ArrayList<Produto>();
+                }
+                //Cria uma instância de Cliente e popula com os valores do BD
+                Produto produto = new Produto();
+                
+                produto.setNome(result.getString("PRODUTO.NOME"));
+                produto.setDescricao(result.getString("DESCRICAO"));
+                produto.setPreco_compra(result.getFloat("PRECO_COMPRA"));
+                produto.setPreco_venda(result.getFloat("PRECO_VENDA"));
+                produto.setQuantidade(result.getInt("QUANTIDADE"));
+                produto.setDt_cadastro(result.getDate("DT_CADASTRO"));
+                produto.setCategoria(result.getString("CATEGORIA.NOME"));
+                
+                
+                listaProduto.add(produto);
+            }
+        } finally {
+            //Se o result ainda estiver aberto, realiza seu fechamento
+            if (result != null && !result.isClosed()) {
+                result.close();
+            }
+            //Se o statement ainda estiver aberto, realiza seu fechamento
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+        //Retorna a lista de clientes do banco de dados
+        return listaProduto;
+    }
+}
---
+++ PG_ADO_01/src/main/java/br/com/ado/model/produto/Produto.java
@@ -0,0 +1,78 @@
+
+package br.com.ado.model.produto;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author victo
+ */
+public class Produto {
+    
+    private String nome;
+    private String descricao;
+    private float preco_compra;
+    private float preco_venda;
+    private int quantidade;
+    private Date dt_cadastro;
+    private String Categoria;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getPreco_compra() {
+        return preco_compra;
+    }
+
+    public void setPreco_compra(float preco_compra) {
+        this.preco_compra = preco_compra;
+    }
+
+    public float getPreco_venda() {
+        return preco_venda;
+    }
+
+    public void setPreco_venda(float preco_venda) {
+        this.preco_venda = preco_venda;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Date getDt_cadastro() {
+        return dt_cadastro;
+    }
+
+    public void setDt_cadastro(Date dt_cadastro) {
+        this.dt_cadastro = dt_cadastro;
+    }
+
+    public String getCategoria() {
+        return Categoria;
+    }
+
+    public void setCategoria(String Categoria) {
+        this.Categoria = Categoria;
+    }
+    
+    
+    
+}
